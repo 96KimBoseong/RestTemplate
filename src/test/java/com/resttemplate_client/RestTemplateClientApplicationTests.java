84b09f8ccbf47eae7b6c80f1d9333ed916b03862
kimboseong <njunju77@nate.com>
2024-07-28 07:47:39 +0900

    init commit

---
+++ src/test/java/com/resttemplate_client/RestTemplateClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttemplate_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
