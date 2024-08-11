9249870e8ab52475ac117c0ccae4efe76348a153
kimboseong <njunju77@nate.com>
2024-08-11 23:47:03 +0900

    RestTemplate get 테스트

--- src/main/java/com/resttemplate_client/controller/RestTemplateController.java
+++ src/main/java/com/resttemplate_client/controller/RestTemplateController.java
@@ -1,9 +1,13 @@
 package com.resttemplate_client.controller;
 
+import com.resttemplate_client.dto.ItemDto;
+import com.resttemplate_client.service.RestTemplateService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
